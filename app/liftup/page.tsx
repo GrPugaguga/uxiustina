ff9737256433ca880bbac30e389ff13a428126a7
GrPugaguga <pugaguga@mail.ru>
2026-01-27 20:35:28 +0500

    add: add liftup page

--- app/liftup/page.tsx
+++ app/liftup/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function LiftUp() {
   return (
-    <div className="min-h-screen bg-background-white pt-32.5 sm:pt-55 px-5 sm:px-25">
-      <h1 className="text-6 font-semibold text-black">LiftUp</h1>
+    <div className="min-h-screen pt-32.5 px-5 sm:px-25 bg-background-white flex flex-col items-center">
+      {/* Hero Image */}
+        <Image
+          src="/image/LiftUp/liftUp_1.png"
+          alt="LiftUp"
+          width={937}
+          height={710}
+          className="w-88.25 h-67 sm:w-234.25 sm:h-177.5 object-cover"
+        />
+
+      {/* Content Block */}
+      <div className="mt-15 sm:mt-30 flex flex-col gap-15 sm:gap-30 w-full items-center">
+
+      </div>
     </div>
   );
 }
