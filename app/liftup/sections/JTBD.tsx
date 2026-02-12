85906477e8defef1bd1ed6760ed304cb193cb63e
GrPugaguga <pugaguga@mail.ru>
2026-02-12 10:06:48 +0500

    fix: fix images names

--- app/liftup/sections/JTBD.tsx
+++ app/liftup/sections/JTBD.tsx
@@ -6,13 +6,13 @@ export default function JTBD() {
     <>
         <ImageCarousel
           images={[
-            "/image/LiftUp/sections/jtbd/Frame-21.png",
-            "/image/LiftUp/sections/jtbd/Frame13829.png",
-            "/image/LiftUp/sections/jtbd/JTBD(Достижение цели).png",
-            "/image/LiftUp/sections/jtbd/JTBD(Незнание функционала).png",
-            "/image/LiftUp/sections/jtbd/JTBD(Отслеживание тренировки в процессе).png",
-            "/image/LiftUp/sections/jtbd/JTBD(Постановка цели).png",
-            "/image/LiftUp/sections/jtbd/JTBD(Преодолеть плато).png"
+            "/image/LiftUp/sections/jtbd/JTBD1.png",
+            "/image/LiftUp/sections/jtbd/JTBD2.png",
+            "/image/LiftUp/sections/jtbd/JTBD3.png",
+            "/image/LiftUp/sections/jtbd/JTBD4.png",
+            "/image/LiftUp/sections/jtbd/JTBD5.png",
+            "/image/LiftUp/sections/jtbd/JTBD6.png",
+            "/image/LiftUp/sections/jtbd/JTBD7.png"
           ]}
           height={650}
           mobileHeight={204}
