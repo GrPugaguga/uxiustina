11bdb5eda827ecddef97440cad4ee5a126654e92
GrPugaguga <pugaguga@mail.ru>
2026-02-24 15:04:34 +0500

    feat(saas): add ProblemAndHypothesis section

--- app/saas/sections/index.ts
+++ app/saas/sections/index.ts
@@ -1,3 +1,4 @@
 export {default as MainDashboard} from "./MainDashboard";
 export {default as RoleAndResearch} from "./RoleAndResearch/RoleAndResearch";
 export {default as Note} from "./Note";
+export {default as ProblemAndHypothesis} from './ProblemAndHypothesis'
