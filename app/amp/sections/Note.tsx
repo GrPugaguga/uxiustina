03d647860eec91c0a1bc27c959e3196ecaad423e
GrPugaguga <pugaguga@mail.ru>
2026-03-17 17:43:28 +0500

    fix(amp: note): add br

--- app/amp/sections/Note.tsx
+++ app/amp/sections/Note.tsx
@@ -4,7 +4,7 @@ export default function Note() {
     <div className="grid grid-cols-8 gap-5 rounded-[40px] bg-linear-to-b from-[#F5F5F500] to-grey-bg-primary py-15">
       <span className="col-start-2 font-semibold text-[24px] leading-6.5 text-black">Примечание</span>
       <span className="col-start-5 col-span-3 font-normal text-[16px] font-sf leading-5.25 tracking-[-0.17px] text-black">
-        {"Проект реализован в\u00A0рамках коммерческой разработки и\u00A0находится под\u00A0NDA. В\u00A0кейсе представлены обезличенные данные, обобщённая логика UX-решений и\u00A0переработанные интерфейсы без\u00A0раскрытия бизнес-процессов и\u00A0технической реализации."}
+        {"Проект реализован в\u00A0рамках коммерческой разработки и\u00A0находится под\u00A0NDA."}<br/> {"В\u00A0кейсе представлены обезличенные данные, обобщённая логика UX-решений и\u00A0переработанные интерфейсы\u00A0без раскрытия бизнес-процессов и\u00A0технической реализации."}
       </span>
     </div>
   );
